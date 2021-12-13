f250d9b7f2a17713551868fcf0e4b8d2adfdcbce
João Ribeiro <Joao.PP.Ribeiro@ctw.bmwgroup.com>
2021-12-13 18:44:19 +0000

    Day 9 part2

--- internal/day9/day9.go
+++ internal/day9/day9.go
@@ -3,6 +3,7 @@ package day9
 import (
 	"aoc-2021/internal/utils"
 	"fmt"
+	"sort"
 	"strconv"
 )
 
@@ -16,6 +17,8 @@ func Solution() {
 	res := part1()
 
 	fmt.Println(res)
+
+	fmt.Println(part2())
 }
 
 func part1() int {
@@ -38,11 +41,91 @@ func part1() int {
 }
 
 func part2() int {
+	lows := lows()
 
+	var basins []int
+
+	for _, l := range lows {
+		n := basin(l)
+
+		basins = append(basins, n)
+	}
+
+	sort.Ints(basins)
+
+	return basins[len(basins)-1] * basins[len(basins)-2] * basins[len(basins)-3]
+}
+
+func lows() [][2]int {
+	top, right, bottom, left := 0, 0, 0, 0
+
+	var lows [][2]int
+
+	for i:=1;i<len(heightMap)-1;i++ {
+		for j:=1;j<len(heightMap[i])-1;j++ {
+			top, right, bottom, left = i-1, j+1, i+1, j-1
+
+			n := heightMap[i][j]
+			if n < heightMap[i][left] && n < heightMap[i][right] && n < heightMap[top][j] && n < heightMap[bottom][j] {
+				lows = append(lows, [2]int{i, j})
+			}
+		}
+	}
+
+	return lows
 }
 
 func basin(low [2]int) int {
+	basin := make([][]int, len(heightMap))
+	for i := range basin {
+		basin[i] = make([]int, len(heightMap[i]))
+	}
 
+	basin[low[0]][low[1]] = 1
+
+	safe := 0
+	next := 1
+
+	for safe != next {
+		safe = next
+		next = 0
+
+		for i := 1; i < len(basin) - 1; i++ {
+			for j := 1; j < len(basin[i]) - 1; j++ {
+				if basin[i][j] == 1 {
+					// left
+					if heightMap[i][j-1] < 9 && basin[i][j-1] < 1 {
+						basin[i][j-1]++
+					}
+
+					// right
+					if heightMap[i][j+1] < 9 && basin[i][j+1] < 1 {
+						basin[i][j+1]++
+					}
+
+					// top
+					if heightMap[i-1][j] < 9 && basin[i-1][j] < 1 {
+						basin[i-1][j]++
+					}
+
+					// bottom
+					if heightMap[i+1][j] < 9 && basin[i+1][j] < 1 {
+						basin[i+1][j]++
+					}
+				}
+			}
+		}
+
+		for _, l := range basin {
+			for _, n := range l {
+				if n > 0 {
+					next += n
+				}
+			}
+		}
+	}
+
+	return next
 }
 
 func parseHeightMap(lines []string) [][]int {
--- internal/day9/day9_test.go
+++ internal/day9/day9_test.go
@@ -66,3 +66,74 @@ func Test_part1(t *testing.T) {
 		})
 	}
 }
+/*
+
+		{0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,1,0,0,0,0,0,0,0,0},
+		{0,0,1,1,1,0,0,0,0,0,0,0},
+		{0,0,0,1,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0},
+
+ */
+func Test_basin(t *testing.T) {
+	heightMap = [][]int{
+		{9,9,9,9,9,9,9,9,9,9,9,9},
+		{9,2,1,9,9,9,4,3,2,1,0,9},
+		{9,3,9,8,7,8,9,4,9,2,1,9},
+		{9,9,8,5,6,7,8,9,8,9,2,9},
+		{9,8,7,6,7,8,9,6,7,8,9,9},
+		{9,9,8,9,9,9,6,5,6,7,8,9},
+		{9,9,9,9,9,9,9,9,9,9,9,9},
+	}
+
+	type args struct {
+		low [2]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"ok", args{[2]int{3,3}}, 14},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := basin(tt.args.low); got != tt.want {
+				t.Errorf("basin() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_lows(t *testing.T) {
+	heightMap = [][]int{
+		{9,9,9,9,9,9,9,9,9,9,9,9},
+		{9,2,1,9,9,9,4,3,2,1,0,9},
+		{9,3,9,8,7,8,9,4,9,2,1,9},
+		{9,9,8,5,6,7,8,9,8,9,2,9},
+		{9,8,7,6,7,8,9,6,7,8,9,9},
+		{9,9,8,9,9,9,6,5,6,7,8,9},
+		{9,9,9,9,9,9,9,9,9,9,9,9},
+	}
+
+	expect := [][2]int{
+		{1,2},
+		{1, 10},
+		{3,3},
+		{5,7},
+	}
+
+	tests := []struct {
+		name string
+		want [][2]int
+	}{
+		{"ok", expect},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lows(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("lows() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
